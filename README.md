37ad2d37a5f6f3ad5b7ef7c74deb29f45ea999a4
resistdesign <ryantgraff@resistdesign.com>
2020-06-21 10:27:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smelted-db
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/smelted-db)
---
+++ index.tsx
@@ -0,0 +1,188 @@
+import "./style.css";
+import React, { useState, useCallback } from "react";
+import { render } from "react-dom";
+import { hot } from "react-hot-loader";
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+type ConnectionMap = {
+  [key: string]: string;
+};
+type Item = {
+  id?: string;
+  value?: string;
+  connections?: ConnectionMap;
+};
+
+// Data
+
+const ValueDBMap = {};
+const ConnectionDBMap = {};
+
+// Get/Set
+
+const getValue = id => ValueDBMap[id];
+const setValue = (id, value: any) => {
+  if (typeof value === "undefined") {
+    delete ValueDBMap[id];
+  } else {
+    ValueDBMap[id] = value;
+  }
+};
+
+// Connections
+
+const getConnectionMap = (itemId): ConnectionMap =>
+  ConnectionDBMap[itemId] || {};
+const setConnection = (itemId, fromId, toId) => {
+  ConnectionDBMap[itemId] = ConnectionDBMap[itemId] || {};
+
+  if (ValueDBMap.hasOwnProperty(fromId) && ValueDBMap.hasOwnProperty(toId)) {
+    ConnectionDBMap[itemId][fromId] = toId;
+  }
+};
+const setConnectionsFromMap = (itemId, connections: ConnectionMap = {}) => {
+  for (const k in connections) {
+    setConnection(itemId, k, connections[k]);
+  }
+};
+const deleteConnections = itemId => {
+  delete ConnectionDBMap[itemId];
+};
+
+// Items
+
+const createItem = (value: string): Item => {
+  const id = uuidv4();
+
+  setValue(id, value);
+
+  return {
+    id,
+    value,
+    connections: {}
+  };
+};
+const readItem = (id): Item => ({
+  id,
+  value: getValue(id),
+  connections: getConnectionMap(id)
+});
+const getConnectedItems = (id): Item[] => {
+  const connectionMap = getConnectionMap(id);
+  const connectedItems = [];
+
+  for (const k in connectionMap) {
+    connectedItems.push(readItem(k));
+  }
+
+  return connectedItems;
+};
+const updateItem = ({ id, value, connections }: Item = {}) => {
+  if (id) {
+    // TODO: Consider a patch method.
+    setValue(id, value);
+    setConnectionsFromMap(id, connections);
+  }
+};
+const deleteItem = id => {
+  setValue(id, undefined);
+  deleteConnections(id);
+};
+
+// Smelted Objects
+
+type SmeltedObject = {
+  id?: string;
+  [keys: string]: string;
+};
+
+const createObject = (
+  obj: SmeltedObject,
+  tags: string[] = [new Date().toISOString()]
+) => {
+  const tagString = tags.join("\n");
+  const { id } = createItem(tagString);
+
+  for (const k in obj) {
+    const value = obj[k];
+    const { id: keyId } = createItem(k);
+    const { id: valueId } = createItem(value);
+
+    setConnection(id, keyId, valueId);
+  }
+
+  return {
+    ...obj,
+    id
+  };
+};
+const readObject = (id: string): SmeltedObject => {
+  const { connections = {} } = readItem(id);
+  const obj: SmeltedObject = {};
+
+  for (const fromId in connections) {
+    const toId = connections[fromId];
+    const { value: k } = readItem(fromId);
+    const { value } = readItem(toId);
+
+    obj[k] = value;
+  }
+
+  obj.id = id;
+
+  return obj;
+};
+
+// Input/Output
+
+const App = () => {
+  const [v, setV] = useState("");
+  const onSubmit = useCallback(
+    e => {
+      const newValue = e.target.v.value;
+
+      e.preventDefault();
+
+      createItem(newValue);
+
+      setV(newValue);
+
+      e.target.reset();
+    },
+    [v, setV]
+  );
+  const onDelete = useCallback(
+    e => {
+      deleteItem(e.target.dataset.itemId);
+
+      setV("");
+    },
+    [v, setV]
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input defaultValue="" name="v" />
+        <button type="submit">Add</button>
+        <br />
+        {Object.keys(ValueDBMap).map(k => (
+          <div key={`Item:${k}`} data-item-id={k} onClick={onDelete}>
+            {ValueDBMap[k]}
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+};
+
+const appDiv: HTMLElement = document.getElementById("root");
+
+render(<App />, appDiv);
